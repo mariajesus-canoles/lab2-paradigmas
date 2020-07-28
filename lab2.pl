2c586cdf84e7c79051f9137891ecdf15d5a34648
Maria Jesus Canoles <maria.canoles.r@usach.cl>
2020-07-28 01:30:48 -0400

    función de pertenencia, selector y modificador del tda repositorio

--- lab2.pl
+++ lab2.pl
@@ -2,17 +2,50 @@ getFecha(Fecha):-get_time(X),convert_time(X,Fecha).
 
 %-----TDA REPOSITORIO-----
 %CONSTRUCTOR
-repositorio(NombreRep,Autor,Fecha,Workspace,Index,Local,Remote,RepoOut):-
+repositorio(NombreRep,Autor,Fecha,Workspace,Index,Local,Remote,RepoOutput):-
     Info=[NombreRep,Autor,Fecha],
-    RepoOut=[Info,Workspace,Index,Local,Remote],!.
+    RepoOutput=[Info,Workspace,Index,Local,Remote],!.
 
 %PERTENENCIA
+isListaStrings([]).
+isListaStrings([Elemento|Cola]):-string(Elemento),isListaStrings(Cola).
+
+isCommits([]).
+isCommits([Elemento|Cola]):-is_list(Elemento),
+    isListaStrings(Elemento),
+    isCommits(Cola).
+
+isRep(RepInput):-is_list(RepInput),
+    length(RepInput,5),
+    RepInput=[InfoRep,Workspace,Index,Local,Remote],
+    is_list(InfoRep),
+    length(InfoRep,3),
+    isListaStrings(InfoRep),
+    is_list(Workspace),
+    isListaStrings(Workspace),
+    is_list(Index),
+    isListaStrings(Index),
+    is_list(Local),
+    isCommits(Local),
+    is_list(Remote),
+    isCommits(Remote).
+
+
 %SELECTORES
-%getNombreRep(RepInput,NombreRep):-
+getInfoRep([Info|_],Info).
+
     
 %MODIFICADORES
+setNombreRep(NombreRep,RepInput,RepoOutput):-
+    getAutorRep(RepInput,AutorRep),
+    getFechaRep(RepInput,FechaRep),
+    getWorkspace(RepInput,Workspace),
+    getIndex(RepInput,Index),
+    getLocal(RepInput,Local),
+    getRemote(RepInput,Remote),
+    RepoOutput=[[NombreRep,AutorRep,FechaRep],Workspace,Index,Local,Remote].
 
 %FUNCIONES OBLIGATORIAS
-gitInit(NombreRep,Autor,RepoOut):-
+gitInit(NombreRep,Autor,RepoOutput):-
     getFecha(Fecha),
-    repositorio(NombreRep,Autor,Fecha,[],[],[],[],RepoOut).
+    repositorio(NombreRep,Autor,Fecha,[],[],[],[],RepoOutput).
